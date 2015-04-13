02b180fe3872521d3e39f8a12048b16e28e4075a
Catherine Johnson <cfjohnson@earthlink.net>
2015-04-12 18:43:50 -0600

    added products and console responses in

--- check-it-out/Program.cs
+++ check-it-out/Program.cs
@@ -11,19 +11,17 @@ namespace checkitout
 			var product1 = new Product1 ();
 			var product2 = new Product2 ();
 			List<int> cart = new List<int>();
-			//var cart = new Cart ();
+
 			int[] products = new int[] {product.price, product1.price, product2.price};
 
 			foreach (int i in products) {
 				cart.Add(i);
+				Console.Write ("you added a product at $" + i + " to your cart\n");
 			}
 
-//			cart.Add(product.price);
-//			cart.Add(3);
-//			cart.Add(5);
-//			cart.Add(7);
 			var Total = new Checkout (cart);
 			var veryTotal = Total.Total;
+			Console.Write ("for a total cost of $" + veryTotal + ".  now pay up!\n");
 		}
 	}
 }
